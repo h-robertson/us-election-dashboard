f460b3e39e3e6d968c586aecaaee67ef78353a23
Helena Robertson <h.robertson@icloud.com>
2020-10-18 22:26:22 +0100

    2016 ec results + current senate breakdown toggle & fill

--- index.js
+++ index.js
@@ -11,8 +11,17 @@ const projDict = {
     "noelectionrep": "#EBEBE8"
 }
 
-var getCol = function (myKey) {
+var projCol = function (myKey) {
     return projDict[myKey];
+};
+
+const resultDict = {
+    "D": "#124683",
+    "R": "#9C1515"
+}
+
+var resultCol = function (myKey) {
+    return resultDict[myKey];
 };
 
 const projCols = [
@@ -120,28 +129,47 @@ d3.json('/data/ec.json').then(function (d) {
         return acc + (cur.ecvs);
     }, 0);
 
-    d.forEach(function (el) {
-        barBase
-            .append('div')
-            .style('height', '40px')
-            .style('width', (el.ecvs / 538 * 100) + "%")
-            .style('background-color', getCol(el.rating))
-            .on("mouseover", function () {
-                d3.select(this).style("border", "1px solid #EBEBE8");
-                presTooltip.transition()
-                    .duration(200)
-                    .style("opacity", 0.9);
-                presTooltip.html(el.state + "<br/>" + el.ecvs + " electoral vote" + (el.ecvs == 1 ? "" : "s"))
+    var projByState = {};
+    d.forEach(el => (projByState[el.state] = projCol(el.rating)));
+    var getProjCol = function (myKey) {
+        return projByState[myKey];
+    };
 
-            })
-            .on("mouseout", function (d) {
-                d3.select(this).style("border", "none");
-                presTooltip.transition()
-                    .duration(400)
-                    .style('opacity', 0)
-            })
-            .attr("class", "state-votes")
-    })
+    var resultsByState = {};
+    d.forEach(el => (resultsByState[el.state] = resultCol(el.result_2016)));
+    var getResultCol = function (myKey) {
+        return resultsByState[myKey];
+    };
+
+    // console.log(projByState)
+    // console.log(resultsByState)
+
+
+    barBase
+        .selectAll('.state-votes')
+        .data(d)
+        .enter()
+        .append('div')
+        .style('height', '40px')
+        .style('width', d => (d.ecvs / 538 * 100) + "%")
+        // console.log((d.ecvs / 538 * 100) + "%")
+        .style('background-color', d => (getProjCol(d.state)))
+        .on("mouseover", function () {
+            d3.select(this).style("border", "1px solid #EBEBE8");
+            presTooltip.transition()
+                .duration(200)
+                .style("opacity", 0.9);
+            presTooltip.html(d.state + "<br/>" + d.ecvs + " electoral vote" + (d.ecvs == 1 ? "" : "s"))
+
+        })
+        .on("mouseout", function (d) {
+            d3.select(this).style("border", "none");
+            presTooltip.transition()
+                .duration(400)
+                .style('opacity', 0)
+        })
+        .attr("class", "state-votes")
+
 
 
     // Pres Map
@@ -162,34 +190,20 @@ d3.json('/data/ec.json').then(function (d) {
     const g = mapSvg
         .append('g')
 
-    var colourByState = {};
-
-
-    d.forEach(el => (colourByState[el.state] = getCol(el.rating)));
-
-    var getStateCol = function (myKey) {
-        return colourByState[myKey];
-    };
-
     d3.json("data/counties-10m.json")
         .then(function (us) {
-
-
             const states = topojson.feature(us, us.objects.states)
 
-
-
-
-            // console.log(colourByState)
             const proj = d3.geoAlbersUsa().scale(900).translate([350.5, 225])
             const path = d3.geoPath()
                 .projection(proj)
+
 
             g.selectAll("path") //this week using paths instead of "rects" to create bar charts
                 .data(states.features) //using .features to bind the data this time. GeoJSON that is an object that has two features, features are what we actually want to access the array of objects
                 .enter() //this then propogates into our group
                 .append("path") //path per geoemtry
-                .attr("fill", d => getStateCol(d.properties.name)) //then applying a fill color. can be hex colors or actual colors
+                .attr("fill", d => getProjCol(d.properties.name)) //then applying a fill color. can be hex colors or actual colors
                 .attr("stroke", "#EBEBE8")
                 .attr("stroke-width", "0.3px") //defining the stroke color, in this case black
                 .attr("d", path)
@@ -199,6 +213,34 @@ d3.json('/data/ec.json').then(function (d) {
                 .on("mouseout", function (d) {
                     d3.select(this).style("stroke-width", "0.3px");
                 })
+
+            $("#pres-toggle :input").on('change', function () {
+                d.sort(function (a, b) {
+                    return a.result_2016 < b.result_2016 ? -1 : a.result_2016 > b.result_2016 ? 1 : 0;
+                });
+
+                d.reduce((acc, cur) => {
+                    cur.start = acc;
+                    return acc + (cur.ecvs);
+                }, 0);
+
+                console.log(d)
+
+
+                g.selectAll("path")
+                    .transition()
+                    .duration(300)
+                    .attr("fill", d => this.id == "pres2020" ? getProjCol(d.properties.name) : getResultCol(d.properties.name))
+
+                barBase
+                    .selectAll('.state-votes')
+                    .data(d)
+                    .transition()
+                    .duration(300)
+                    .style('width', d => (d.ecvs / 538 * 100) + "%")
+                    .style('background-color', d => this.id == "pres2020" ? getProjCol(d.state) : getResultCol(d.state))
+
+            })
         });
 })
 
@@ -239,12 +281,29 @@ senateLeg
 // Waffle chart example: https://bl.ocks.org/JulienAssouline/b98116bb991e13beb5418c45a2e64a14
 
 var wafWidth = 800
-var wafHeight = 400
+var wafHeight = 200
 var numRows = 5
 
 var senWaf = d3.select('.sen-graphics')
     .append('div')
     .attr('class', 'sen-waffle d-flex justify-content-center')
+
+senWaf
+    .append('div')
+    .html('<b>50</b>')
+    .style('position', 'absolute')
+    .style('font-size', '1.2em')
+    .style('left', '48.6%')
+    .style('top', '72px')
+
+senWaf
+    .append('div')
+    .style('height', "220px")
+    .style('width', '2px')
+    .style('background-color', '#EBEBE8')
+    .style('position', 'absolute')
+    .style('left', '49.5%')
+    .style('top', '100px')
 
 var senTooltip = senWaf
     .append("div")
@@ -262,13 +321,22 @@ var senWafSvg = senWaf
 
 
 d3.json('/data/senate.json').then(function (data) {
+    console.log(data)
 
-    var colourByState = {}
+    var projByState = {}
 
-    data.forEach(el => (colourByState[el.state_unique] = getCol(el.projection.replace('_', ' '))));
+    data.forEach(el => (projByState[el.state_unique] = projCol(el.projection.replace('_', ' '))));
 
-    var getStateCol = function (myKey) {
-        return colourByState[myKey];
+    var getProjCol = function (myKey) {
+        return projByState[myKey];
+    };
+
+    var curByState = {}
+
+    data.forEach(el => (curByState[el.state_unique] = resultCol(el.held)));
+
+    var getCurCol = function (myKey) {
+        return curByState[myKey];
     };
 
     senWafSvg
@@ -288,8 +356,7 @@ d3.json('/data/senate.json').then(function (data) {
             var colIndex = Math.floor(i / numRows)
             return colIndex * 40
         })
-        .attr("r", 6)
-        .style("fill", d => getStateCol(d.state_unique))
+        .style("fill", d => getProjCol(d.state_unique))
         .on("mouseover", function (d) {
             senTooltip.transition()
                 .duration(100)
@@ -299,7 +366,7 @@ d3.json('/data/senate.json').then(function (data) {
                 .style("stroke-width", "3px")
             senTooltip
                 .html(d.state_unique)
-            console.log(d) //can't access data?? 'd' here = mouseevent not data...
+            // console.log(d) //can't access data?? 'd' here = mouseevent not data...
         })
 
         .on("mouseout", function (d) {
@@ -309,9 +376,28 @@ d3.json('/data/senate.json').then(function (data) {
             d3.select(this).style("stroke", "none")
         })
 
-    // var senCurrent = document.getElementById('senCur')
-    //     .onclick(function () {
-    //     })
+    $("#sen-toggle :input").on('change', function () {
+        // console.log(data)
+        data.sort(function (a, b) {
+            return a.held < b.held ? -1 : a.held > b.held ? 1 : 0;
+        })
+        console.log(data)
+
+        senWafSvg
+            .selectAll("rect")
+            .data(data)
+            .transition()
+            .duration(300)
+            .attr("y", function (d, i) {
+                var rowIndex = i % numRows
+                return rowIndex * 40
+            })
+            .attr("x", function (d, i) {
+                var colIndex = Math.floor(i / numRows)
+                return colIndex * 40
+            })
+            .style("fill", d => this.id == "sen2020" ? getProjCol(d.state_unique) : getCurCol(d.state_unique))
+    })
 })
 
 
